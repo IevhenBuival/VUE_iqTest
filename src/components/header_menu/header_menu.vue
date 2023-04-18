6921a1bf8bd6352cb744953e7bc39ebaf5cd2ab2
IevhenBuival <kdotbull@gmail.com>
2023-04-18 18:23:15 +0300

    save 1

---
+++ src/components/header_menu/header_menu.vue
@@ -0,0 +1,17 @@
+<template>
+  <header_title
+    v-for="item of menu_list"
+    :key="item.id"
+    v-bind:itemtext="item"
+  />
+</template>
+
+<script>
+import header_title from "@/components/header_menu/menu_item/header_title";
+export default {
+  props: ["menu_list"],
+  components: {
+    header_title,
+  },
+};
+</script>
